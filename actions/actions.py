effaf132d1f0f6ac48ed3b4e61d7ce3f50e7d6f3
Akshay Dodkade <akshaydodkade@gmail.com>
2025-07-02 19:09:26 +0530

    chatbot - enhance time recongnisation

--- actions/actions.py
+++ actions/actions.py
@@ -5,8 +5,11 @@ from rasa_sdk.events import SlotSet
 class ActionBookMeeting(Action):
   def name(self) -> str:
     return "action_book_meeting"
-  
+
   def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> list:
     time = tracker.get_slot("time")
-    dispatcher.utter_message(response="utter_confirm_booking", time=time)
+    if time:
+      dispatcher.utter_message(response="utter_confirm_booking", time=time)
+    else:
+      dispatcher.utter_message(text="Sorry, I couldn’t understand the time. Could you clarify?")
     return [SlotSet("time", time)]
